ca3181336ecd6276377cbce7c28d262c22e20175
Renee Fowler <reneemichellefowler@gmail.com>
2018-02-27 19:52:52 -0800

    fixes to case study media queries

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-    // Slick Slider
-    $('.testimonial-wrapper').slick({
-        adaptiveHeight: true,
-        dots: true
-    });
-
     // Mobile Navigation
     $('.fa-bars').click(function() {
         $(this).toggleClass('fa-window-close');
         $('.nav-links-mobile').toggleClass('expanded');
+    });
+
+    // Slick Slider
+    $('.testimonial-wrapper').slick({
+        adaptiveHeight: true,
+        dots: true
     });
 });
